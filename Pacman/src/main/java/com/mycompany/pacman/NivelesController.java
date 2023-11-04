376af997a1b0a8954f0766dfdddddce361cb4bf1
Kimberly-Porras <89230963+Kimberly-Porras@users.noreply.github.com>
2023-11-03 21:31:07 -0600

    Niveles

--- Pacman/src/main/java/com/mycompany/pacman/NivelesController.java
+++ Pacman/src/main/java/com/mycompany/pacman/NivelesController.java
@@ -6,7 +6,11 @@ package com.mycompany.pacman;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,6 +19,11 @@ import javafx.fxml.Initializable;
  */
 public class NivelesController implements Initializable {
 
+    @FXML
+    private Button bntNIvel10;
+    @FXML
+    private ImageView btnRegresar;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +31,41 @@ public class NivelesController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void bntNIvel1(ActionEvent event) {
+    }
+
+    @FXML
+    private void bntNIvel2(ActionEvent event) {
+    }
+
+    @FXML
+    private void bntNIvel3(ActionEvent event) {
+    }
+
+    @FXML
+    private void bntNIvel4(ActionEvent event) {
+    }
+
+    @FXML
+    private void bntNIvel5(ActionEvent event) {
+    }
+
+    @FXML
+    private void bntNIvel6(ActionEvent event) {
+    }
+
+    @FXML
+    private void bntNIvel7(ActionEvent event) {
+    }
+
+    @FXML
+    private void bntNIvel8(ActionEvent event) {
+    }
+
+    @FXML
+    private void bntNIvel9(ActionEvent event) {
+    }
     
 }
