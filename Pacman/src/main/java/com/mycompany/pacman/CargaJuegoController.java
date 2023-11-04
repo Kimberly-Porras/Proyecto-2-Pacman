edee3b94a79b8decad54d71f787604bf8ea2b9c6
Yellowoe <joxanportilla@gmail.com>
2023-11-03 21:26:49 -0600

    Pantalla Inicio

--- Pacman/src/main/java/com/mycompany/pacman/CargaJuegoController.java
+++ Pacman/src/main/java/com/mycompany/pacman/CargaJuegoController.java
@@ -1,37 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package com.mycompany.pacman;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
 
+/**
+ * FXML Controller class
+ *
+ * @author joxan
+ */
 public class CargaJuegoController implements Initializable {
 
-    @FXML
-    private ProgressBar progressBar;
-
-    @FXML
-    private Button startButton;
-
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Inicializar la barra de progreso y el botón
-        progressBar.setProgress(0.0);
-
-        startButton.setOnAction(e -> iniciarProceso());
-    }
-
-    private void iniciarProceso() {
-        // Simulación de un proceso que toma tiempo
-        for (int i = 0; i <= 100; i++) {
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-            progressBar.setProgress((double) i / 100);
-        }
-    }
+        // TODO
+    }    
+    
 }
